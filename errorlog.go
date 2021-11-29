eaa10f14e43ae753d85a36c19262d2f51e102fcc
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-11-30 02:50:25 +0800

    Modify ErrorLog struct to use pointer type to allow null/optional fields

--- errorlog.go
+++ errorlog.go
@@ -3,18 +3,19 @@ package main
 import "time"
 
 type ErrorLog struct {
-	ID           int
-	Time         time.Time
-	RequestURL   string
-	StackTrace   string
-	UserAgent    string
-	HTTPCode     int
-	AppName      string
-	FunctionName string
+	ID           *int       `json:"id"`
+	Time         *time.Time `json:"time"`
+	RequestURL   *string    `json:"request_url"`
+	StackTrace   *string    `json:"stack_trace"`
+	UserAgent    *string    `json:"user_agent"`
+	HTTPCode     *int       `json:"http_code"`
+	AppName      *string    `json:"app_name"`
+	FunctionName *string    `json:"function_name"`
 }
 
 type ErrorLogStore interface {
 	ErrorLog(id int) (ErrorLog, error)
+	ErrorLogByURL(url string) ([]ErrorLog, error)
 	ErrorLogByApp(appName string) ([]ErrorLog, error)
 	ErrorLogByFunction(functionName string) ([]ErrorLog, error)
 	ErrorLogs() ([]ErrorLog, error)
