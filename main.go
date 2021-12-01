0e5eec2a0b061c5cf210a99f1ee954d79f5ee2b0
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-12-01 17:12:17 +0800

    Test

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	r.HandleFunc("/logs/{id}", env.getLogHandler).Methods("GET")
 	r.HandleFunc("/chat", env.setChatIDHandler).Methods("POST")
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Testt!")
+		fmt.Fprintf(w, "Test123!")
 	})
 
 	err = http.ListenAndServe(":8080", r)
