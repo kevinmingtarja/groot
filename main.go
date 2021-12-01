6f8e4690e379c35ae4c8debcc4b3670fccf35a42
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-12-01 17:32:07 +0800

    Verify ci/cd

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	r.HandleFunc("/logs/{id}", env.getLogHandler).Methods("GET")
 	r.HandleFunc("/chat", env.setChatIDHandler).Methods("POST")
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Test123!")
+		fmt.Fprintf(w, "Hello!")
 	})
 
 	err = http.ListenAndServe(":8080", r)
